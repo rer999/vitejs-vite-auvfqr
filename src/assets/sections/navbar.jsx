78fc367a8bd0a83e09e4cbdca20023d0038f7661
rer999 <128755603+rer999@users.noreply.github.com>
2023-09-04 21:41:29 +0100

    4 files added

---
+++ src/assets/sections/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className="">
+        <div className="navbar"></div>
+      </div>
+    </>
+  );
+};
