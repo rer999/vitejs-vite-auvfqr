489cd7652121fa09cbdf83d5ab81dfb5f2d4693e
rer999 <128755603+rer999@users.noreply.github.com>
2023-09-04 22:04:01 +0100

    3 files modified, 5 added, 3 deleted

---
+++ src/sections/footer.tsx
@@ -0,0 +1,21 @@
+export const Footer = () => {
+  return (
+    <>
+      <div>
+        <div className="">
+          <div>
+            <a href="" className="">
+              <h1>Home</h1>
+            </a>
+            <a href="" className="">
+              <h1>Profile</h1>
+            </a>
+            <a href="" className="">
+              <h1>About</h1>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
